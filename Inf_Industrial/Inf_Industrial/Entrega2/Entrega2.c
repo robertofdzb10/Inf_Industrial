274e80262ec25cba806c2c00d2c481b499f56d71
robertofdzb10 <roberto.fernandez@opendeusto.es>
2023-02-27 10:06:24 +0100

    Update

--- Inf_Industrial/Inf_Industrial/Entrega2/Entrega2.c
+++ Inf_Industrial/Inf_Industrial/Entrega2/Entrega2.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-FILE *archivo; //Puntero FILE para apuntar al archivo que se va a leer      
+#include <assert.h>
 
-int main(){ 
-    archivo = fopen("DatosConfig.txt", "r"); //Esta función devuele un puntero al fichero indicado. El segundo parámetro módo de apertura, r es para lectura
-    if (archivo == NULL){
-        printf("La apertura del archivo fallo");
-    } else{
-        printf("La apertura del archivo fue satisfactoría");
-    }
-    //LeeConfig(archivo);
-    fclose(archivo); //Cerrramos el archivo
-    return 0;
+int main(){
+    LeeConfig("DatosConfig.txt");
+    return 0; // En el main si devuelvo 1 indicamos error. 
 }
 
-void* LeeConfig(char *fichero){ //*void, puntero genérico, es decir que la función puede devolver cualquier tipo de puntero
-
+void *LeeConfig(char *fichero){  // *void, puntero genérico, es decir que la función puede devolver cualquier tipo de puntero     
+    FILE *archivo; // Puntero FILE para apuntar al archivo que se va a leer                                        
+    archivo = fopen(fichero, "r"); // Esta función devuele un puntero al fichero indicado. El segundo parámetro módo de apertura, r es para lectura
+    if (archivo == NULL){
+        printf("La apertura del archivo fallo");
+        return NULL; // Solo puedo devolver punteros o NULL
+    }
+    fclose(archivo); // Cerrramos el archivo
 }
