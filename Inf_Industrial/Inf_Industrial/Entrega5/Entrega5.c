5de84b7b40893359f82aac6a6d977d5f5acd4a44
robertofdzb10 <roberto.fernandez@opendeusto.es>
2023-03-13 17:30:22 +0100

    Update

--- Inf_Industrial/Inf_Industrial/Entrega5/Entrega5.c
+++ Inf_Industrial/Inf_Industrial/Entrega5/Entrega5.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Datos.h"
 
 char recogeDatos(double *tiempo, int *valores, int *n);
 
 // Valor entero si ha recogido un nuevo bloque 
-// -1 si no hay nuevos valores
+// -1 si no hay nuevos valores  
 // -2 sino devuelve nada y no hay datos de más piezas
 
 // tiempo, intervalo de timepo que ha tardado el fabricantee en el lote actual
@@ -13,19 +15,46 @@ char recogeDatos(double *tiempo, int *valores, int *n);
 int main()
 {
     double *tiempo;
-    int *valores, *n;
+    int *valores, *n; // Valores lo hago un puntero por que no se su tamaño
     int contador = 0;
 
+    Datos *datos = (Datos*)malloc(sizeof(Datos));
+    if (datos == NULL){
+        printf("Error reservando memoria.");
+        return 1;
+    }
+
+
     // TODO borrar
+    /*
     double prueba = 1.111111; 
     int prueba_1 = 1;
     tiempo = &prueba;
     valores = &prueba;
+    */
     // TODO borrar
 
-    // recogeDatos(tiempo, valores,n);
-    
 
+    while (recogeDatos(tiempo, valores, n) != -2){ // Mientras haya valores para devolver
+        datos[contador].tiempo_d = tiempo; // Puntero con puntero ¿?
+        datos[contador].n_d = n;
+        if (n < 20){
+            printf("Error, se han introducido características de más de la pieza.");
+            return 1;
+        }
+        datos[contador].valores_d = (Datos*)malloc(*n * sizeof(int));
+        if (datos == NULL || datos[contador].valores_d == NULL){
+            printf("Error reservando memoria.");
+            return 1;
+        }
+        for (int i = 0; i < n; i++){
+            datos->valores_d[i] = valores;
+        }
+        contador ++;
+        
+    }
+
+    //Preparación de archivo
     FILE *archivo;
     archivo = fopen("datos.text", "a");
     if (archivo == NULL){
@@ -33,10 +62,20 @@ int main()
         return 1;
     }
 
-    fprintf(archivo, "%03d: %8.3f segundos\n", (contador + 1), *tiempo); // 03 rellena con tres 0s, 8.3 imprime 8 carácteres en total 3 de los cuales en la parte decimal
-    fprintf(archivo, "0x%X\n", *valores); // Valores en hexadecimal válidos
-    fprintf(archivo, "Decimal 1\n");
-    fprintf(archivo, "Decimal 2\n\n");
+    //Escritura de datos en el archivo
+    for (int i = 0; i < contador; i++){
+        fprintf(archivo, "%03d: %8.3f segundos\n", (contador + 1), datos[i].tiempo_d); // 03 rellena con tres 0s, 8.3 imprime 8 carácteres en total 3 de los cuales en la parte decimal
+        for (int i = 0; i < datos[i].n_d; i++){
+            fprintf(archivo, "0x%X ", *valores); // Valores en hexadecimal válidos
+        }
+        fprintf(archivo, "\nDecimal 1\n");
+        fprintf(archivo, "Decimal 2\n\n");
+    }
+
+    // Librerar memoria y cerrar archivos
+    fclose(archivo);
+    free(datos->valores_d); // Vale con liberar en general valores_d
+    free(datos);
 
     return 0;
 }
